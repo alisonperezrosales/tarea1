08956aa5a92f416274d8597e81118a9a4bd80cb7
alisonperezrosales <2556156630101@ingenieria.usac.edu.gt>
2024-02-19 12:41:33 -0600

    first commit

---
+++ ejemplo1/ejemplo1/Program.vb
@@ -0,0 +1,15 @@
+Imports System
+
+Module Program
+    Sub Main()
+        Dim mensaje, Nombre As String
+        Console.Clear()
+        Console.Write("Ingrese nombre, numero de carnet")
+        Nombre = Console.ReadLine()
+        Console.Clear()
+        mensaje = "Hola mundo"
+        Console.WriteLine(mensaje + Nombre + "2024")
+        Console.WriteLine("presione cualquier tecla")
+        Console.ReadKey()
+    End Sub
+End Module
